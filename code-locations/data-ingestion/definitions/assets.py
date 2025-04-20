861769046fa7c062020606d5337dd0e94ab02f52
Jesufemi-O <ogunwedeemmanuel@gmail.com>
2025-04-20 03:40:50 +0100

    first commit

---
+++ code-locations/data-ingestion/definitions/assets.py
@@ -0,0 +1,10 @@
+from dagster import asset
+import time
+
+@asset
+def long_running_asset():
+    for i in range(12):
+        print(f"Running batch {i+1}/12...")
+        # simulate some work
+        time.sleep(30)
+    print("✅ long_running_asset completed.")
